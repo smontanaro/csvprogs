3a0a2bd3de6751196335653ad5324b0a1372c2e4
Skip Montanaro <skip.montanaro@gmail.com>
2021-02-21 13:41:33 -0600

    Python3 only, getopt -> argparse

--- csvprogs/src/html2csv.py
+++ csvprogs/src/html2csv.py
@@ -37,9 +37,9 @@ SEE ALSO
 
 """
 
+import argparse
 import csv
 import datetime
-import getopt
 from html.parser import HTMLParser
 import os
 import sys
@@ -47,36 +47,28 @@ import sys
 PROG = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 EPOCH = datetime.datetime.fromtimestamp(0)
 
-def main(args):
-    table = 1
-    verbose = False
-    try:
-        opts, args = getopt.getopt(args, "ht:v")
-    except getopt.GetoptError:
-        usage()
-        return 1
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--table", dest="table", default=1,
+                        type=int)
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False,
+                        action="store_true")
+    (options, args) = parser.parse_known_args()
 
-    for opt, arg in opts:
-        if opt == "-h":
-            usage()
-            return 0
-        if opt == "-t":
-            table = int(arg)
-        elif opt == "-v":
-            verbose = True
+    inf = open(args[0]) if len(args) >= 1 else sys.stdin
+    outf = open(args[1], "w") if len(args) == 2 else sys.stdout
 
-    if args:
-        usage()
-        return 1
-
-    parser = TableParser(table, verbose)
-    for line in sys.stdin:
-        parser.feed(line)
-    wrtr = csv.writer(sys.stdout)
-    wrtr.writerows(parser.rows)
+    tbl_parser = TableParser(options.table, options.verbose)
+    for line in inf:
+        tbl_parser.feed(line)
+    tbl_parser.close()
+    wrtr = csv.writer(outf)
+    for row in tbl_parser.rows:
+        wrtr.writerow(row)
     return 0
 
 class TableParser(HTMLParser):
+    "Extract row/cell info from the given table number."
     def __init__(self, table_num, verbose):
         self.table_num = table_num
         self.verbose = verbose
@@ -111,7 +103,7 @@ class TableParser(HTMLParser):
             if data not in ("\r\n", "\n", "\r"):
                 self.rows[-1].append(data)
 
-    def handle_table(self, what, tag, attrs):
+    def handle_table(self, what, _tag, _attrs):
         if what == "start":
             self.table_num -= 1
             if not self.table_num:
@@ -120,7 +112,7 @@ class TableParser(HTMLParser):
         if not self.table_num:
             self.processing_table = False
 
-    def handle_tr(self, what, tag, attrs):
+    def handle_tr(self, what, _tag, _attrs):
         if self.processing_table:
             if what == "start":
                 self.rows.append([])
@@ -128,10 +120,6 @@ class TableParser(HTMLParser):
     def error(self, message):
         print(message, file=sys.stderr)
 
-def usage(msg=""):
-    if msg:
-        print(msg.rstrip(), file=sys.stderr)
-    print(__doc__ % globals(), file=sys.stderr)
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
