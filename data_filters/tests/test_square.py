629b4758a7371947b253e839d738756891b50457
Skip Montanaro <skip.montanaro@gmail.com>
2024-12-08 19:41:28 -0600

    remove list test, react to simpler square() function

--- data_filters/tests/test_square.py
+++ data_filters/tests/test_square.py
@@ -2,7 +2,7 @@
 
 import datetime
 
-from square import square
+from data_filters.square import square
 
 def test_dict_square():
     rows = [
@@ -53,63 +53,11 @@ def test_dict_square():
             "y": 1.90,
             },
         ]
-    result = list(square(iter(rows), "time", "y", False))
-    assert result == exp_rows, result
-
-def test_list_square():
-    rows = [
-        [
-            "2010-01-01T00:00:00",
-            1.90,
-            ],
-        [
-            "2010-01-01T00:00:01",
-            1.94,
-            ],
-        [
-            "2010-01-01T00:00:02",
-            1.88,
-            ],
-        [
-            "2010-01-01T00:00:03",
-            1.90,
-            ],
-        ]
-    exp_rows = [
-        [
-            "2010-01-01T00:00:00",
-            1.90,
-            ],
-        [
-            "2010-01-01T00:00:01",
-            1.90,
-            ],
-        [
-            "2010-01-01T00:00:01",
-            1.94,
-            ],
-        [
-            "2010-01-01T00:00:02",
-            1.94,
-            ],
-        [
-            "2010-01-01T00:00:02",
-            1.88,
-            ],
-        [
-            "2010-01-01T00:00:03",
-            1.88,
-            ],
-        [
-            "2010-01-01T00:00:03",
-            1.90,
-            ],
-        ]
-    result = list(square(iter(rows), 0, 1, False))
+    result = list(square(iter(rows), ["time"]))
     assert result == exp_rows, result
 
 def test_empty_square():
     rows = []
     exp_rows = []
-    result = list(square(iter(rows), "time", "y", False))
+    result = list(square(iter(rows), ["y"]))
     assert result == exp_rows, result
