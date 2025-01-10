d8b47e726cfad025e5f5f76dc05539a841ac5036
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-09 18:43:37 -0600

    correct several mistakes

--- tests/test_csv2csv.py
+++ tests/test_csv2csv.py
@@ -1,6 +1,8 @@
 import csv
 import io
 
+from csvprogs.csv2csv import csv2csv
+
 INPUT = """\
 time,close,position\r
 2015-04-15T12:00,,1\r
@@ -26,24 +28,28 @@ time,close\r
 """
 
 TAB_EXPECTED = """\
-time,close,position\r
-2015-04-15T12:00,,1\r
-2015-04-15T12:30,,-1\r
-2015-04-15T12:45,,1\r
-2015-04-15T14:45,,-1\r
-2015-04-15T15:00,26.98,\r
-2015-04-16T09:30,,1\r
-2015-04-16T15:00,27.04,\r
-2015-04-17T15:00,27.77,\r
+time\tclose\tposition\r
+2015-04-15T12:00\t\t1\r
+2015-04-15T12:30\t\t-1\r
+2015-04-15T12:45\t\t1\r
+2015-04-15T14:45\t\t-1\r
+2015-04-15T15:00\t26.98\t\r
+2015-04-16T09:30\t\t1\r
+2015-04-16T15:00\t27.04\t\r
+2015-04-17T15:00\t27.77\t\r
 """
 
 def test_csv2csv():
-    inp = csv.DictReader(io.StringIO(INPUT))
-    out = csv.DictWriter(io.StringIO(), fieldnames=["time", "close"])
-    csvprogs.csv2csv.csv2csv(inp, out, out.fieldnames)
+    rdr = csv.DictReader(io.StringIO(INPUT))
+    out = io.StringIO()
+    wtr = csv.DictWriter(out, fieldnames=["time", "close"])
+    wtr.writeheader()
+    csv2csv(rdr, wtr, wtr.fieldnames)
     assert out.getvalue() == COMMA_EXPECTED
 
-    inp = csv.DictReader(io.StringIO(INPUT))
-    out = csv.DictWriter(io.StringIO(), delimiter="\t", fieldnames=inp.fieldnames)
-    csvprogs.csv2csv.csv2csv(inp, out, inp.fieldnames)
+    rdr = csv.DictReader(io.StringIO(INPUT))
+    out = io.StringIO()
+    wtr = csv.DictWriter(out, delimiter="\t", fieldnames=rdr.fieldnames)
+    wtr.writeheader()
+    csv2csv(rdr, wtr, wtr.fieldnames)
     assert out.getvalue() == TAB_EXPECTED
