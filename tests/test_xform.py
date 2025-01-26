5bab657baf60fae270753fd6deb64df3927327d7
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-26 11:44:57 -0600

    coverage

--- tests/test_xform.py
+++ tests/test_xform.py
@@ -4,7 +4,6 @@ import csv
 import io
 import os.path
 import subprocess
-import sys
 import tempfile
 import unittest
 
@@ -28,6 +27,19 @@ def xform(row):
     return (pre, [])
 """
 
+        callstring = """
+from datetime import datetime, timedelta
+
+class XForm:
+    def __call__(self, row):
+        t = datetime.strptime(row["time"], "%Y-%m-%dT%H:%M")
+        pre_t = t - timedelta(seconds=1)
+        pre = [{"time": pre_t.strftime("%Y-%m-%dT%H:%M:%S"),
+                "close": row["close"] - 0.01}]
+        return (pre, [])
+xform = XForm()
+"""
+
         expected = b"""\
 time,close\r
 2015-04-15T14:59:59,26.97\r
@@ -38,29 +50,29 @@ time,close\r
 2015-04-17T15:00,27.77\r
 """
 
-        cwd = os.getcwd()
         inp = io.StringIO()
         writer = csv.writer(inp)
         writer.writerows(input_list)
         raw_input = inp.getvalue()
-        with tempfile.NamedTemporaryFile(mode="w+", dir="/tmp",
-                                         suffix=".py") as xfile:
-            xfile.file.write(modstring)
-            xfile.file.flush()
-            xfile.seek(0)
-            modname = os.path.splitext(os.path.split(xfile.name)[1])[0]
-            env = dict(os.environ)
-            env["PYTHONPATH"] = "/tmp"
-            result = subprocess.run(
-                [
-                 "./venv/bin/python", "-m",
-                 "csvprogs.xform",
-                 "-F", f"{modname}.xform",
-                 "-p", "sample_global=1.0"
-                ],
-                env=env, stdout=subprocess.PIPE, stderr=None,
-                input=bytes(raw_input, encoding="utf-8"))
-            assert result.stdout == expected
+        for s in (modstring, callstring):
+            with tempfile.NamedTemporaryFile(mode="w+", dir="/tmp",
+                                             suffix=".py") as xfile:
+                xfile.file.write(s)
+                xfile.file.flush()
+                xfile.seek(0)
+                modname = os.path.splitext(os.path.split(xfile.name)[1])[0]
+                env = dict(os.environ)
+                env["PYTHONPATH"] = "/tmp"
+                result = subprocess.run(
+                    [
+                     "./venv/bin/python", "-m",
+                     "csvprogs.xform",
+                     "-F", f"{modname}.xform",
+                     "-p", "sample_global=1.0"
+                    ],
+                    env=env, stdout=subprocess.PIPE, stderr=None,
+                    input=bytes(raw_input, encoding="utf-8"))
+                assert result.stdout == expected
 
 if __name__ == "__main__":
     unittest.main()
