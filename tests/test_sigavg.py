90b2d4114b26c2d5d6cc86a70118816d9deb018f
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-28 16:41:07 -0600

    use -m/-M args

---
+++ tests/test_sigavg.py
@@ -0,0 +1,28 @@
+import csv
+import io
+import subprocess
+
+from tests import SPY_CSV
+
+
+def test_cli():
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.sigavg",
+                            "-x", "Date", "-y", "Close",
+        "-m", "447.75", "-M", "448.75", SPY_CSV],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert result.returncode == 0
+
+    inf = csv.DictReader(io.StringIO(result.stdout.decode("utf-8")))
+    rows = list(inf)
+    assert rows[0] == {
+        "time": "13:30:00",
+        "mean": "447.94",
+        "sum": "895.88",
+        "n": "2",
+        }
+    assert rows[-1] == {
+        "time": "19:50:00",
+        "mean": "447.95",
+        "sum": "447.95",
+        "n": "1",
+        }
