9e514aa8be1dc342e3cb99b67c7299ee524dc208
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-28 13:38:09 -0600

    use csvprogs.common stuff, require headers on input/output

--- csvprogs/spline.py
+++ csvprogs/spline.py
@@ -19,29 +19,20 @@ compute parametric spline of a series of values
 SYNOPSIS
 ========
 
-  %(PROG)s [ -f x ] [ -s sep ] [ -m val ] [ -M val ]
+Compute a smoothed spline of the Close values in somefile.csv with time as the
+independent variable.
 
-OPTIONS
-=======
-
--f x     compute spline of the values in column x (zero-based offset, default 1)
--s sep   use sep as the field separator (default is comma)
--m val   discard values below this value (no default)
--M val   discard values above this value (no default)
--F fmt   use this format to parse timestamps (default is to use dateutil.parser)
+  %(PROG)s -x time -f Close -c spline --smooth=1 somefile.csv
 
 DESCRIPTION
 ===========
 
-Data are read from stdin, spline values are added to the end as a new
+Data are read from stdin or file, spline values are added to the end as a new
 column.
 
 Note that by default $(PROG)s uses dateutil.parser.parse to parse
-timestamps. This is robust, but quite slow for large datasets. If your
-timestamps are all (or mostly) in a single format it's better to use
-the -F fmt command line option. Even if your data are not all in the
-same format this can help. If parsing using the given format fails,
-the code falls back to dateutil.parser.parse anyway.
+timestamps. This is robust, but quite slow for large datasets.
+
 
 SEE ALSO
 ========
@@ -55,85 +46,64 @@ SEE ALSO
 * sigavg
 """
 
-import sys
-import getopt
-import os
 import csv
+import os
+import sys
 import time
-import datetime
 
 import numpy
 from scipy import interpolate
 import dateutil.parser
 
-from csvprogs.common import usage
+from csvprogs.common import CSVArgParser, usage, openio
 
 PROG = os.path.basename(sys.argv[0])
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], "f:s:hm:M:TtS:F:")
+    parser = CSVArgParser(usage=usage(__doc__, globals()))
+    parser.add_argument("-f", "--field", required=True,
+                        help="field/column used as input to spline function")
+    parser.add_argument("-c", "--column", default="spline",
+                        help="column used as output of the spline function")
+    parser.add_argument("-s", "--smooth", default=0, type=float,
+                        help="smoothing parameter")
+    parser.add_argument("-x", default="time",
+                        help="default column for independent data")
+    parser.add_argument("--x-not-time", dest='istime', default=True,
+                        action='store_false',
+                        help="indicate independent variable is not time-based")
+    (options, args) = parser.parse_known_args()
 
-    field = None
-    sep = ","
-    maxval = 1e308
-    minval = -1e308
-    xtime = True
-    smooth = 0
-    xfmt = ""
-    for opt, arg in opts:
-        if opt == "-f":
-            field = int(arg)
-        elif opt == "-T":
-            xtime = False
-        elif opt == "-t":
-            xtime = True
-        elif opt in ("-m", "--minval"):
-            minval = float(arg)
-        elif opt in ("-M", "--maxval"):
-            maxval = float(arg)
-        elif opt == "-s":
-            sep = arg
-        elif opt == "-S":
-            smooth = int(arg)
-        elif opt == "-F":
-            xfmt = arg
-        elif opt == "-h":
-            print(usage(__doc__, globals()), file=sys.stderr)
-            return 0
+    mode = "a" if options.append else "w"
+    with openio(args[0] if len(args) >= 1 else sys.stdin, "r",
+                args[1] if len(args) == 2 else sys.stdout, mode,
+                encoding=options.encoding) as (inf, outf):
+        rdr = csv.DictReader(inf, delimiter=options.insep)
+        wtr = csv.DictWriter(outf, fieldnames=rdr.fieldnames+[options.column],
+            delimiter=options.outsep)
+        if not options.append:
+            wtr.writeheader()
 
-    if field is None:
-        print(usage(__doc__, globals(), msg="field number is required"),
-                    file=sys.stderr)
-        return 1
+        rows = list(rdr)
+        if options.istime:
+            x = [to_timestamp(dateutil.parser.parse(row[options.x]))
+                    for row in rows]
+        else:
+            x = [float(row[0]) for row in rows]
+        x = numpy.array(x, dtype=float)
+        y = numpy.array([float(row[options.field]) for row in rows], dtype=float)
+        tck, u = interpolate.splprep([x, y], s=options.smooth)
+        ynew = interpolate.splev(u, tck)
+        for (y, row) in zip(ynew[1], rows):
+            row[options.column] = y
+        wtr.writerows(rows)
 
-    rows = list(csv.reader(sys.stdin, delimiter=sep))
-    if xtime:
-        x = [to_timestamp(parse_timestamp(row[0], xfmt)) for row in rows]
-    else:
-        x = [float(row[0]) for row in rows]
-    x = numpy.array(x, dtype=float)
-    y = numpy.array([float(row[field]) for row in rows], dtype=float)
-    tck, u = interpolate.splprep([x, y], s=smooth)
-    ynew = interpolate.splev(u, tck)
-    for (y, row) in zip(ynew[1], rows):
-        row.append(y)
-    csv.writer(sys.stdout, delimiter=sep).writerows(rows)
     return 0
-
-def parse_timestamp(stamp, xfmt=""):
-    if xfmt:
-        try:
-            return datetime.datetime.strptime(stamp, xfmt)
-        except ValueError:
-            pass
-
-    # Fall back to the more general dateutil parser if no format was
-    # given or we failed to successfully parse with the given format.
-    return dateutil.parser.parse(stamp)
 
 def to_timestamp(dt):
     """Convert a datetime object into seconds since the Unix epoch."""
     return time.mktime(dt.timetuple()) + dt.microsecond/1e6
+
 
 if __name__ == "__main__":
     sys.exit(main())
