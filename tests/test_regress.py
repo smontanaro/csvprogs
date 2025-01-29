f80044e49d013de5feff9914c07ab2865aa873dc
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-29 15:05:20 -0600

    some regress tests

---
+++ tests/test_regress.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"regress tests"
+
+import csv
+import io
+import subprocess
+import unittest
+
+from tests import NVDA
+
+EPS = 1e-7
+
+
+def test_cli_c():
+    with open(NVDA, "rb") as nvda:
+        raw_input = nvda.read()
+        result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.regress",
+            "-f", "bid,ask", "--corr", NVDA, "/dev/stdout"], check=True,
+            stdout=subprocess.PIPE, stderr=None)
+    assert result.returncode == 0
+    corr = float(result.stdout.strip())
+    assert abs(corr - 0.98374687) < EPS
+
+def test_cli_csv():
+    with open(NVDA, "rb") as nvda:
+        result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.regress",
+            "-f", "bid,ask", "--column", "reg"], check=True,
+            stdout=subprocess.PIPE, stderr=None,
+            input=nvda.read())
+    assert result.returncode == 0
+    output = list(csv.DictReader(io.StringIO(result.stdout.decode("utf-8"))))
+    assert (abs(float(output[7]["reg"]) - 135.4728217) < EPS and
+            abs(float(output[-1]["reg"]) - 137.7006059) < EPS)
+
+
+if __name__ == "__main__":
+    unittest.main()
