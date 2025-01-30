a5666ba16259b8d8ac11fd383c398aae2ba67977
Skip Montanaro <skip.montanaro@gmail.com>
2025-01-29 20:53:08 -0600

    simple test of csv2xls

---
+++ tests/test_csv2xls.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import tempfile
+import xml.etree.ElementTree
+import zipfile
+
+from tests import VRTX_CSV
+
+
+def test_cli():
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csv2xls", VRTX_CSV],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert result.returncode == 0
+    fd, fname = tempfile.mkstemp()
+    try:
+        with os.fdopen(fd, mode="wb") as fp:
+            fp.write(result.stdout)
+        with zipfile.ZipFile(fname, "r") as zf:
+            with zf.open("xl/worksheets/sheet1.xml") as sheet:
+                tree = xml.etree.ElementTree.parse(sheet)
+                root = tree.getroot()
+                sheet = root.findall(".")[0]
+                assert list(sheet[4][0].itertext()) == ["Date","Open","High",
+                                                        "Low","Close","% Change",
+                                                        "% Change vs Average","Volume"]
+    finally:
+        os.unlink(fname)
