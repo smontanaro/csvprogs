16e43ecf708112e68985f443b62dc433a0a77bdf
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-01 12:01:17 -0600

    simple test for mean

---
+++ tests/test_mean.py
@@ -0,0 +1,20 @@
+import csv
+import io
+import subprocess
+
+from tests import VRTX_DAILY
+
+EPS = 1e-7
+
+
+def test_cli():
+    with open(VRTX_DAILY, "rb") as vrtx:
+        vrtx_data = vrtx.read()
+
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.mean",
+        "-f", "% Change", "-m", "-10", "-M", "+10"],
+        stdout=subprocess.PIPE, stderr=None, input=vrtx_data)
+    assert result.returncode == 0
+    values = [float(v) for v in result.stdout.decode("utf-8").split(",")]
+
+    assert values == [3845.0, 0.043686, 0.049400, 2.095824]
