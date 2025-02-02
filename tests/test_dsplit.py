817de35df55ff19ac8d50f76b72ec0b86efef141
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-02 14:57:41 -0600

    adapt to csvprogs.common

--- tests/test_dsplit.py
+++ tests/test_dsplit.py
@@ -8,7 +8,7 @@ def test_cli():
     with open(SPLIT_IN, "rb") as inp:
         inp_data = inp.read()
 
-    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.dsplit", "-v", "weight"],
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.dsplit", "-c", "weight"],
         stdout=subprocess.PIPE, stderr=None, input=inp_data)
     assert result.returncode == 0
     act = list(csv.DictReader(io.StringIO(result.stdout.decode("utf-8"))))
