12c6e26eb45dfb9cfcc5d1691ff0ec466beb77e8
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-02 14:00:08 -0600

    only save data within <th> or <td> tags, add some verbose indentation, etc

--- csvprogs/html2csv.py
+++ csvprogs/html2csv.py
@@ -75,30 +75,46 @@ class TableParser(HTMLParser):
         self.handlers = {
             "table": self.handle_table,
             "tr": self.handle_tr,
+            "th": self.handle_cell,
+            "td": self.handle_cell,
             }
-        self.processing_table = False
+        self._processing_table = False
+        self._processing_cell = False
+        self._indent = 0
         super().__init__()
+
+    @property
+    def processing_table(self):
+        return self._processing_table
+    @processing_table.setter
+    def processing_table(self, value):
+        self._processing_table = value
+
+    @property
+    def processing_cell(self):
+        return self._processing_cell
+    @processing_cell.setter
+    def processing_cell(self, value):
+        self._processing_cell = value
 
     def handle_starttag(self, tag, attrs):
         handler = self.handlers.get(tag)
         if handler is None:
-            if self.verbose:
-                print("Start", tag)
+            self.error("start", tag, attrs)
             return
         handler("start", tag, attrs)
+        self._indent += 2
 
     def handle_endtag(self, tag):
         handler = self.handlers.get(tag)
         if handler is None:
-            if self.verbose:
-                print("End", tag)
+            self.error("end", tag)
             return
+        self._indent -= 2
         handler("end", tag, [])
 
     def handle_data(self, data):
-        if self.processing_table:
-            if self.verbose:
-                print("Found some data:", repr(data))
+        if self.processing_table and self.processing_cell:
             if data not in ("\r\n", "\n", "\r"):
                 self.rows[-1].append(data)
 
@@ -107,6 +123,7 @@ class TableParser(HTMLParser):
             self.table_num -= 1
             if not self.table_num:
                 self.processing_table = True
+                self.error("start table")
             return
         if not self.table_num:
             self.processing_table = False
@@ -115,9 +132,16 @@ class TableParser(HTMLParser):
         if self.processing_table:
             if what == "start":
                 self.rows.append([])
+        self.error(f"{what} tr")
 
-    def error(self, message):
-        print(message, file=sys.stderr)
+    def handle_cell(self, what, tag, _attrs):
+        if self.processing_table:
+            self.processing_cell = what == "start"
+            self.error(f"{what} {tag}", end=" " if what == "start" else "\n")
+
+    def error(self, *args, file=sys.stderr, **kwds):
+        if self.verbose:
+            print(" " * self._indent, *args, file=file, **kwds)
 
 
 if __name__ == "__main__":
