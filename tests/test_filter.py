c24e47afa0aebe8a143f9e6f22eef6d0f05da117
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-02 08:49:03 -0600

    adapt to csvprogs.common, header-only CSV files, bad lambda key column

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -9,9 +9,18 @@ def test_cli():
         ex_data = ex.read()
 
     result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.filter",
-        "-H", "-f", 'lambda row: row["batch"] != "b=1"'],
+        "-f", 'lambda row: row["batch"] != "b=1"', "-k", "lambda-key"],
         stdout=subprocess.PIPE, stderr=None, input=ex_data)
     assert result.returncode == 0
     rdr = csv.DictReader(io.StringIO(result.stdout.decode("utf-8")))
     for row in rdr:
-        assert row["batch"] == "b=0"
+        assert row["batch"] == "b=0" and row["lambda-key"] == "True", row
+
+def test_bad_lambda_key():
+    with open(BATCH_EX, "rb") as ex:
+        ex_data = ex.read()
+
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.filter",
+        "-f", 'lambda row: row["batch"] != "b=1"', "-k", "age"],
+        stdout=subprocess.PIPE, stderr=None, input=ex_data)
+    assert result.returncode != 0
