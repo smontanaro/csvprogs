2ad838d9e82d8e88a94e4f82efd076d3738e3b38
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-03 14:29:44 -0600

    use csvprogs.common functions, make days an option

--- csvprogs/sharpe.py
+++ csvprogs/sharpe.py
@@ -44,45 +44,34 @@ SEE ALSO
 """
 
 import csv
-import getopt
 import math
 import os
 import sys
 
+from csvprogs.common import CSVArgParser, openio, usage
+
+
 PROG = os.path.basename(sys.argv[0])
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], "s:h")
+    parser = CSVArgParser(usage=usage(__doc__, globals()))
+    parser.add_argument("-d", "--days", default=253, type=int,
+                        help="measurements per year")
+    options, args = parser.parse_known_args()
 
-    days = 253
-    sep = ","
-    for opt, arg in opts:
-        if opt == "-s":
-            sep = arg
-        elif opt == "-h":
-            usage()
-            return 0
+    mode = "a" if options.append else "w"
+    with openio(args[0] if len(args) >= 1 else sys.stdin, "r",
+                args[1] if len(args) == 2 else sys.stdout, mode,
+                encoding=options.encoding) as (inf, outf):
+        fields = next(csv.reader(inf, delimiter=options.insep))
 
-    if len(args) > 1:
-        usage(f"{PROG} accepts zero or one positional arguments")
-        return 1
+        _n = int(fields[0])
+        mean = float(fields[1])
+        _median = float(fields[2])
+        std = float(fields[3])
 
-    if args:
-        days = int(args[0])
+        print(mean / std * math.sqrt(options.days), file=outf)
 
-    fields = next(csv.reader(sys.stdin, delimiter=sep))
-
-    n = int(fields[0])
-    mean = float(fields[1])
-    median = float(fields[2])
-    std = float(fields[3])
-    print(mean / std * math.sqrt(days))
-
-def usage(msg=None):
-    if msg is not None:
-        print(msg, file=sys.stderr)
-        print(file=sys.stderr)
-    print((__doc__.strip() % globals()), file=sys.stderr)
 
 if __name__ == "__main__":
     sys.exit(main())
