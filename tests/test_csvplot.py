56c7ef77c18a6510d40197a639a0a504c3cf2027
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-04 16:01:46 -0600

    increase coverage

--- tests/test_csvplot.py
+++ tests/test_csvplot.py
@@ -1,31 +1,47 @@
 import csv
+import os
 import subprocess
-import time
+import tempfile
 
-from matplotlib import pyplot
 from csvprogs.csvplot import plot, Options
-from tests import NVDA, VRTX_CSV
+from tests import NVDA, VRTX_CSV, RANDOM_CSV
 
 def test_plot():
-    options = Options()
-    options.fields = [["time", "last", "l", "red", "legend", "-", ""]]
-
-    with open(NVDA, "r", encoding="utf-8") as inp:
-        rdr = csv.DictReader(inp)
-        plot(options, rdr, block=False)
-        time.sleep(0.25)
-        pyplot.close()
+    fd, fname = tempfile.mkstemp()
+    try:
+        options = Options()
+        options.fields = [["time", "last", "l", "red", "legend", "-", ""]]
+        options.x_label = "X axis label"
+        options.block = False
+        options.plot_file = fname
+        with open(NVDA, "r", encoding="utf-8") as inp:
+            rdr = csv.DictReader(inp)
+            plot(options, rdr, block=False)
+    finally:
+        os.unlink(fname)
 
 def test_cli():
+    env = os.environ.copy()
+    try:
+        del env["DISPLAY"]
+    except KeyError:
+        pass
     result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvplot", "-l", "legend",
         '-f', 'time,last,l,r', '-f', 'time,bid,r,b', "--noblock", "--left_label", "left",
-        "-b", "time,last,135.07,lightgreen", "--format", "%Y-%m"],
-        stdout=subprocess.PIPE, stderr=None, input=open(NVDA, "rb").read())
+        "--right_label", "right", "-b", "time,last,135.07,lightgreen",
+        "--format", "%Y-%m", NVDA],
+        stdout=subprocess.PIPE, stderr=None, env=env)
+    assert result.returncode == 0
+
+def test_cli_nondate():
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvplot",
+        '-f', 'i,random', "--noblock", "--format", '%.0f', RANDOM_CSV],
+        stdout=subprocess.PIPE, stderr=None)
     assert result.returncode == 0
 
 def test_cli_bg():
     result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvplot", "-L",
-        '-f', 'Date,Close,l,r', "-b", "Date,% Change,-1:+1,lightgreen", "--noblock",
-        "--right_label", "right", "-B", "Agg"],
-        stdout=subprocess.PIPE, stderr=None, input=open(VRTX_CSV, "rb").read())
+        "--verbose", '-f', 'Date,Close,l,r', "-b", "Date,% Change,-1:+1,lightgreen",
+        "--noblock", "--right_label", "right", "-B", "Agg", VRTX_CSV],
+        stdout=subprocess.PIPE, stderr=None)
     assert result.returncode == 0
