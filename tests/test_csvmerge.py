ceec712e0edc801e5d9f78806fdaad64f226f4a1
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-25 17:53:00 -0600

    a bit more coverage

--- tests/test_csvmerge.py
+++ tests/test_csvmerge.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-from tests import FIRST, SECOND, MERGED
+from tests import FIRST, SECOND, MERGED, SECOND_SHORT, MERGED_SHORT, EMPTY
 
 def test_bad_cli():
     result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvmerge",
@@ -14,4 +14,18 @@ def test_merge():
         "-k", "time", FIRST, SECOND], stdout=subprocess.PIPE, stderr=None)
     assert result.returncode == 0
     with open(MERGED, "rb") as merged:
+        assert result.stdout == merged.read()
+
+def test_merge_empty():
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvmerge",
+        "-k", "time", FIRST, EMPTY], stdout=subprocess.PIPE, stderr=None)
+    assert result.returncode == 0
+    with open(FIRST, "rb") as merged:
+        assert result.stdout.replace(b"\r\n", b"\n") == merged.read()
+
+def test_merge_short():
+    result = subprocess.run(["./venv/bin/python", "-m", "csvprogs.csvmerge",
+        "-k", "time", FIRST, SECOND_SHORT], stdout=subprocess.PIPE, stderr=None)
+    assert result.returncode == 0
+    with open(MERGED_SHORT, "rb") as merged:
         assert result.stdout == merged.read()
