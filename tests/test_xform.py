1e56d1d22f3c55fe63376f3d1bbc0864d2b1815d
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-28 08:06:07 -0600

    paramet[e]rized test_basic_xform, dump unittest

--- tests/test_xform.py
+++ tests/test_xform.py
@@ -5,54 +5,10 @@ import io
 import os.path
 import subprocess
 import tempfile
-import unittest
+
+import pytest
 
 from tests import RANDOM_CSV, VRTX_CSV
-
-
-class XFormTest(unittest.TestCase):
-    def test_basic_xform(self):
-        input_list = [
-            ["time", "close"],
-            ["2015-04-15T15:00", "26.98"],
-            ["2015-04-16T15:00", "27.04"],
-            ["2015-04-17T15:00", "27.77"],
-        ]
-
-        expected = b"""\
-time,close,name1,name2\r
-2015-04-15T14:59:59,26.97,,\r
-2015-04-15T15:00,26.98,,\r
-2015-04-16T14:59:59,27.029999999999998,,\r
-2015-04-16T15:00,27.04,,\r
-2015-04-17T14:59:59,27.759999999999998,,\r
-2015-04-17T15:00,27.77,,\r
-"""
-
-        inp = io.StringIO()
-        writer = csv.writer(inp)
-        writer.writerows(input_list)
-        raw_input = inp.getvalue()
-        for s in (MODSTRING, CALLSTRING):
-            with tempfile.NamedTemporaryFile(mode="w+", dir="/tmp",
-                                             suffix=".py") as xfile:
-                xfile.file.write(s)
-                xfile.file.flush()
-                xfile.seek(0)
-                modname = os.path.splitext(os.path.split(xfile.name)[1])[0]
-                env = dict(os.environ)
-                env["PYTHONPATH"] = "/tmp"
-                result = subprocess.run(
-                    [
-                     "./venv/bin/python", "-m",
-                     "csvprogs.xform",
-                     "-F", f"{modname}.xform",
-                     "-p", "sample_global=1.0",
-                     "-c", "name1,name2",
-                    ],
-                    env=env, stdout=subprocess.PIPE, stderr=None,
-                    input=bytes(raw_input, encoding="utf-8"))
-                assert result.stdout == expected
 
 
 MODSTRING = """
@@ -105,6 +61,66 @@ class XForm:
 xform = XForm()
 """
 
+METHSTRING = """
+from datetime import datetime, timedelta
+
+class XForm:
+    def xform(self, row):
+        t = datetime.strptime(row["time"], "%Y-%m-%dT%H:%M")
+        pre_t = t - timedelta(seconds=1)
+        pre = [{"time": pre_t.strftime("%Y-%m-%dT%H:%M:%S"),
+                "close": row["close"] - 0.01}]
+        return (pre, [])
+xf = XForm()
+xform = xf.xform
+"""
+
+
+@pytest.mark.parametrize(("xstring",),
+                         [(MODSTRING,), (CALLSTRING,), (METHSTRING,),])
+def test_basic_xform(xstring):
+    input_list = [
+        ["time", "close"],
+        ["2015-04-15T15:00", "26.98"],
+        ["2015-04-16T15:00", "27.04"],
+        ["2015-04-17T15:00", "27.77"],
+    ]
+
+    expected = b"""\
+time,close,name1,name2\r
+2015-04-15T14:59:59,26.97,,\r
+2015-04-15T15:00,26.98,,\r
+2015-04-16T14:59:59,27.029999999999998,,\r
+2015-04-16T15:00,27.04,,\r
+2015-04-17T14:59:59,27.759999999999998,,\r
+2015-04-17T15:00,27.77,,\r
+"""
+
+    inp = io.StringIO()
+    writer = csv.writer(inp)
+    writer.writerows(input_list)
+    raw_input = inp.getvalue()
+    with tempfile.NamedTemporaryFile(mode="w+", dir="/tmp",
+                                     suffix=".py") as xfile:
+        xfile.file.write(xstring)
+        xfile.file.flush()
+        xfile.seek(0)
+        modname = os.path.splitext(os.path.split(xfile.name)[1])[0]
+        env = dict(os.environ)
+        env["PYTHONPATH"] = "/tmp"
+        result = subprocess.run(
+            [
+             "./venv/bin/python", "-m",
+             "csvprogs.xform",
+             "-F", f"{modname}.xform",
+             "-p", "sample_global=1.0",
+             "-c", "name1,name2",
+            ],
+            env=env, stdout=subprocess.PIPE, stderr=None,
+            input=bytes(raw_input, encoding="utf-8"))
+        assert result.stdout == expected
+
+
 def test_f_xform():
     with open(VRTX_CSV, "rb") as f:
         data = f.read()
@@ -147,6 +163,3 @@ def test_missing_xform():
                             ],
         stdout=subprocess.PIPE, stderr=None)
     assert result.stdout != 0
-
-if __name__ == "__main__":
-    unittest.main()
